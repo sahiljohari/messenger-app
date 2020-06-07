5b891cbc8f2ad59fbd35ada671e96a9389264a68
sahiljohari <darewithsj@gmail.com>
2020-06-07 16:45:09 -0400

    feat: color themes (#8)
    * Color themes updated
    
    * Theming complete and ready for review

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -2,12 +2,13 @@ import React, { useContext } from "react";
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { ChatContext } from "../ChatContext/ChatContext";
 import { clientUrls } from "../../clientUrls";
+import styles from "./InfoBar.module.css";
 import messages from "./messages";
 
 const InfoBar = () => {
   const { userName, chatRoom } = useContext(ChatContext);
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar className={styles.root} bg="dark" variant="dark">
       <Navbar.Brand>{chatRoom}</Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-end">
